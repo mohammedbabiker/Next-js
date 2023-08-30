f1fc9a3a8e06b86140d3ae8af2357648db0fd682
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-30 23:05:03 +0300

    use client on one components & data.js | button

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,12 +1,22 @@
-import React from "react";
 import Link from "next/link";
+import { links } from "./data";
+import Button from "../Button/Button";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <div className={Styles.container}>
+    <div className={styles.container}>
       <Link href="/" className={styles.logo}>
         Teashop
       </Link>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
+        ))}
+        <Button />
+      </div>
     </div>
   );
 }
