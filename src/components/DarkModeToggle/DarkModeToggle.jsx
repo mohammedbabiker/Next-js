2fc007488e9cf7c9ab65ca09e59d02238801d00f
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-09-01 09:53:35 +0300

    update nav button & darkModeToggler

---
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -0,0 +1,14 @@
+import styles from "./darkModeToggle.module.css";
+export default function DarkModeToggle() {
+  const mode = "dark";
+  return (
+    <div className={styles.container}>
+      <div className={styles.icon}>🌙</div>
+      <div className={styles.icon}>🌚</div>
+      <div
+        className={styles.switcher}
+        style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+      />
+    </div>
+  );
+}
--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { links } from "./data";
-import Button from "../../elements/Button/Button";
+// import Button from "../../elements/Button/Button";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 import Logo from "../../elements/Logo/Logo";
 import styles from "./Navbar.module.css";
 
@@ -9,12 +10,13 @@ export default function Navbar() {
     <div className={styles.container}>
       <Logo />
       <div className={styles.links}>
+        <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
-        <Button />
+        <button className={styles.logout}>Logout</button>
       </div>
     </div>
   );
