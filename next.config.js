c38152f45abc4f05d909635353db5f19fdbe8bb3
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-09-07 20:03:45 +0300

    update two

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
+    loader: "custom",
     domains: ["images.pexels.com", "i.dummyjson.com", "flaticon.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
   },
+  output: "export",
 };
 
 module.exports = nextConfig;
